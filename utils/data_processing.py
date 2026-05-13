8343e105864b3d8985f643459e808644d0b12d34
Epot12 <e.lorenzo0203@gmail.com>
2026-05-13 11:04:58 +0200

    Backup commit

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 
-def load_and_preprocess_data(dataset_path="MARSIS_historical_dataset.csv", orbit_path="orbit_to_remove"):
+def load_and_preprocess_data(dataset_path="MARSIS_historical_dataset.csv", orbit_path="orbit_to_remove", keep_flux=False):
     """
     Loads the dataset, applies the necessary filters, and removes any excluded columns.
     Returns the feature matrix X and the target vector y.
@@ -27,12 +27,21 @@ def load_and_preprocess_data(dataset_path="MARSIS_historical_dataset.csv", orbit
     df['FM_data_solar_longitude_sin'] = np.sin(df['FM_data_solar_longitude'])
 
     # 5. Eliminating unnecessary columns
-    X_df = df.drop(columns=[
-        'FM_data_ephemeris_time', 'FM_data_F10_7_index', 'FM_data_frequency',
-        'FM_data_median_corrected_echo_power', 'FM_data_orbit_number',
-        'FM_data_peak_corrected_echo_power', 'FM_data_peak_distorted_echo_power',
-        'FM_data_peak_simulated_echo_power', 'FM_data_solar_longitude'
-    ])
+    if keep_flux:
+        X_df = df.drop(columns=[
+            'FM_data_ephemeris_time', 'FM_data_frequency',
+            'FM_data_median_corrected_echo_power', 'FM_data_orbit_number',
+            'FM_data_peak_corrected_echo_power', 'FM_data_peak_distorted_echo_power',
+            'FM_data_peak_simulated_echo_power', 'FM_data_solar_longitude'
+        ])
+
+    else:
+        X_df = df.drop(columns=[
+            'FM_data_ephemeris_time', 'FM_data_F10_7_index', 'FM_data_frequency',
+            'FM_data_median_corrected_echo_power', 'FM_data_orbit_number',
+            'FM_data_peak_corrected_echo_power', 'FM_data_peak_distorted_echo_power',
+            'FM_data_peak_simulated_echo_power', 'FM_data_solar_longitude'
+        ])
 
     # 6. Extracting to NumPy array format
     X = X_df.to_numpy()
