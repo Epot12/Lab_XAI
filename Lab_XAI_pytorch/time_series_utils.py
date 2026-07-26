79a0ce1cce634a3b1986b3cf75a4056939f7e46f
Epot12 <e.lorenzo0202@gmail.com>
2026-07-26 16:37:52 +0200

    Linear regression for solar flux prediction executed.

--- Lab_XAI_pytorch/time_series_utils.py
+++ Lab_XAI_pytorch/time_series_utils.py
@@ -2,15 +2,15 @@ import numpy as np
 
 def create_dataset_windows(data, target_col, time_col, lookback=12):
     """
-    Crea finestre in cui X contiene il flusso e il mese dei passati 'lookback' mesi,
-    e y contiene il flusso del mese successivo.
+    Creates windows where X contains the flow and the month for the past 'lookback' months,
+    and y contains the flow for the following month.
     """
     X_flux, X_time, y = [], [], []
     for i in range(len(data) - lookback):
-        # Finestra storica dei passati W mesi fino al mese corrente
+        # Historical window covering the past W months up to the current month.
         X_flux.append(data[target_col].iloc[i:(i + lookback)].values)
         X_time.append(data[time_col].iloc[i:(i + lookback)].values)
-        # Target: mese successivo
+        # Target: following month
         y.append(data[target_col].iloc[i + lookback])
         
     return np.array(X_flux), np.array(X_time), np.array(y)
